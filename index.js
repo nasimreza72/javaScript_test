0275a9d8087f154339f74d656ef7e64beb580a8f
Nasim <nasimreza72@yahoo.com>
2022-11-21 16:26:13 +0100

    updated solution

--- index.js
+++ index.js
@@ -326,3 +326,20 @@ const user = {
 delete user.profession
 
 console.log(user())
+
+/////////////////////////////
+
+First Factorial
+
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+function FirstFactorial(num) {
+  let array = [];
+  let count = 0
+
+  for (let i = 0; i < num; i++) {
+    count++
+    array.push(num+1 - count)
+  }
+  return array.reduce((a, b) => a * b, 1);
+}
